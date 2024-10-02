fd46f8e51e902bba7e403c11881875ce26b21970
varunreddy9553 <136957672+varunreddy9553@users.noreply.github.com>
2024-10-02 10:49:12 +0530

    Add files via upload
    This files contains all the code and their functionality of the sample quora posts

---
+++ index.js
@@ -0,0 +1,80 @@
+const express=require("express");
+const app=express();
+const port=8080;
+const path=require("path");
+const {v4:uuidv4 }=require('uuid');
+const methodOverride=require("method-override");
+uuidv4();
+
+app.use(express.urlencoded({extended:true }));
+app.use(methodOverride("_method"));
+
+app.get("/posts",(req,res)=>{
+    res.render("index.ejs",{posts});
+})
+app.get("/posts/new",(req,res)=>{
+    res.render("new.ejs");
+})
+app.post("/posts",(req,res)=>{
+   let {username,content} =req.body;
+   let id=uuidv4();
+   posts.push({id,username,content});
+    res.redirect("/posts");
+});
+app.get("/posts/:id",(req,res)=>{
+    let {id}=req.params;8
+    console.log(id);
+    let post=posts.find((p)=> id === p.id);
+    console.log(post);
+    res.render("show.ejs",{post});
+});
+
+app.patch("/posts/:id",(req,res)=>{
+    let {id}=req.params;
+    let newContent=req.body.content;
+    let post=posts.find((p)=> id === p.id);
+    post.content=newContent;
+    console.log(post);
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit",(req,res) => {
+    let {id} =req.params;
+    let post=posts.find((p)=> id ===p.id);
+    res.render("edit.ejs",{post});
+
+})
+app.delete("/posts/:id", (req,res)=> {
+    let { id }=req.params;
+     posts=posts.filter((p)=> id !== p.id);
+    res.redirect("/posts");
+})
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.static(path.join(__dirname,"public")));
+
+
+let posts=[
+    { 
+        id:uuidv4(),
+        username:"apnacollege",
+        content:"I love coding!"
+        
+        
+    },
+     {
+        id:uuidv4(),
+        username:"varun reddy",
+        content:"Hardwork is key to acheive Success!",
+    },
+     {
+        id:uuidv4(),
+        username:"Mahender Reddy",
+        content:"I got selected in 1st Placement Drive"
+    },
+]
+
+app.listen(port,()=>{
+    console.log("listening to port :8080")
+})
